9ba5ef1596d298dc4c73d16c18814dcb5c1b5d57
heavykisiel <nikolas99@wp.pl>
2021-11-19 09:20:43 +0100

    ost zajecia

--- ww/angularv1/src/app/wydawcy/wydawcy.component.ts
+++ ww/angularv1/src/app/wydawcy/wydawcy.component.ts
@@ -1,18 +1,39 @@
-import { Component, OnInit } from '@angular/core';
-import  wydawcy  from '../../assets/wydawcy.json'
-@Component({
-  selector: 'app-wydawcy',
-  templateUrl: './wydawcy.component.html',
-  styleUrls: ['./wydawcy.component.css']
-})
-export class WydawcyComponent implements OnInit {
-  wydawcy!:any[];
-  index:number = 0;
+  import { Component, OnInit } from '@angular/core';
+  import  wydawcyRaw  from '../../assets/wydawcy.json'
+  @Component({
+    selector: 'app-wydawcy',
+    templateUrl: './wydawcy.component.html',
+    styleUrls: ['./wydawcy.component.css']
+  })
+  export class WydawcyComponent implements OnInit {
+    wydawcy!:any[];
+    wydawcyRaw!:any[];
 
-  constructor() { }
+    index:number = 0;
 
-  ngOnInit(): void {
-    this.wydawcy = wydawcy.slice(this.index,this.index+10)
+    constructor() { }
+
+    ngOnInit(): void {
+      this.wydawcy = wydawcyRaw.slice(this.index,this.index+10)
+    }
+    next(){
+      this.index+=10;
+      this.wydawcy = wydawcyRaw.slice(this.index,this.index+10)
+    }
+    back(){
+      this.index-=10;
+      this.wydawcy = wydawcyRaw.slice(this.index,this.index+10)
+    }
+    changeO(obj:any){
+      this.wydawcy=wydawcyRaw;
+      if(obj.target.value == "asc"){
+        
+        this.wydawcy.sort((a,b)=> a["Pe\u0142na nazwa wydawcy"] > b["Pe\u0142na nazwa wydawcy"] ? 1 : -1 )
+        this.wydawcy = this.wydawcy.slice(this.index,this.index+10)
+      }else if(obj.target.value == "desc"){
+        this.wydawcy.sort((a,b)=> a["Pe\u0142na nazwa wydawcy"] < b["Pe\u0142na nazwa wydawcy"] ? 1 : -1 )
+        this.wydawcy = this.wydawcy.slice(this.index,this.index+10)
+      }
+      //alert(obj.target.value);
+    }
   }
-
-}
